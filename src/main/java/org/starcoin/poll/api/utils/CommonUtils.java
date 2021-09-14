8d21de08fcbdf0c48a4526546c83516326068f26
yangjiefeng <yangjiefeng@gmail.com>
2021-09-14 15:04:37 +0800

    更新了 VoteChangedEvent（原结构字段有误）、重构，及其他小问题。

--- src/main/java/org/starcoin/poll/api/utils/CommonUtils.java
+++ src/main/java/org/starcoin/poll/api/utils/CommonUtils.java
@@ -1,5 +1,7 @@
 package org.starcoin.poll.api.utils;
 
+import java.util.List;
+
 public class CommonUtils {
 
     public static byte hexToByte(String inHex) {
@@ -26,5 +28,38 @@ public class CommonUtils {
             j++;
         }
         return result;
+    }
+
+    public static String byteListToHexWithPrefix(List<Byte> bytes) {
+        return "0x" + byteListToHex(bytes);
+    }
+
+    public static String byteListToHex(List<Byte> bytes) {
+        byte[] byteArray = toPrimitive(bytes.toArray(new Byte[0]));
+        return byteArrayToHex(byteArray);
+    }
+
+    public static byte[] toPrimitive(final Byte[] array) {
+        if (array == null) {
+            return null;
+        } else if (array.length == 0) {
+            return new byte[0];
+        }
+        final byte[] result = new byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i].byteValue();
+        }
+        return result;
+    }
+
+    public static String byteArrayToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (int index = 0, len = bytes.length; index <= len - 1; index += 1) {
+            String hex1 = Integer.toHexString((bytes[index] >> 4) & 0xF);
+            String hex2 = Integer.toHexString(bytes[index] & 0xF);
+            result.append(hex1);
+            result.append(hex2);
+        }
+        return result.toString();
     }
 }
